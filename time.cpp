adcbeb6bbee8c34bfbfea764732fff85e60ee505
Matt Roth <mrothhb@gmail.com>
2020-09-08 16:54:26 -0700

    latest time

--- time.cpp
+++ time.cpp
@@ -2,76 +2,68 @@ class Solution {
 public:
     string largestTimeFromDigits(vector<int>& arr) {
         
-        bool invalid = true;
-        bool second_max = false;
-        std::vector<int>::iterator it; 
-        vector<int> arr_copy; 
-        string time = "";
-        arr_copy = arr;
+        string str = "";
         
-        for(int i = 2; i >= 0; i--){
-            it = std::find (arr_copy.begin(), arr_copy.end(), i);
-            if (it != arr_copy.end()){
-                if( i == 2 )
-                    second_max = true;
-                time+= std::to_string(i);
-                arr_copy.erase(it);
-                invalid = false;
-                break;
-            }
+        for(int i = 0; i<arr.size(); i++){
+            str+=to_string(arr[i]);
         }
-        
-        if(invalid)
-            return "";
-        
-        if(!second_max){
-            for(int i = 9; i >= 0; i--){
-            it = std::find (arr_copy.begin(), arr_copy.end(), i);
-            if (it != arr_copy.end()){
-                time+= std::to_string(i);
-                arr_copy.erase(it);
-                break;
-            }
-        }
-        invalid = false;
-            
-        } else{
-            
-         invalid = true; 
-         for(int i = 3; i >= 0; i--){
-            it = std::find (arr_copy.begin(), arr_copy.end(), i);
-            if (it != arr_copy.end()){
-                time+= std::to_string(i);
-                arr_copy.erase(it);
-                invalid = false;
-                break;
-            }
-        }
-            
-        }
-        
-        if(invalid)
-            return "";
-        invalid = true; 
-        time+=":";
-        
-        for(int i = 5; i >= 0; i--){
-            it = std::find (arr_copy.begin(), arr_copy.end(), i);
-            if (it != arr_copy.end()){
-                time+= std::to_string(i);
-                arr_copy.erase(it);
-                invalid = false;
-                break;
-            }
-        }
-        
-        if(invalid)
-            return "";
-        
-        time+= std::to_string(arr_copy[0]);
-        
-        
-        return time;
-        
+ 
+    return permute(str);
     }
+    
+    // Function to print permutations of string str 
+// using next_permutation 
+string permute(string str) 
+{ 
+    vector<string> strings;
+    vector<string> finals;
+    vector<int> numbers;
+    int tempInt = 0;
+    string newStr = "";
+    std::string finalstr= str;
+    // Sort the string in lexicographically 
+    // ascennding order 
+    sort(str.begin(), str.end()); 
+  
+    // Keep printing next permutation while there 
+    // is next permutation 
+    do { 
+      // cout << str << endl;
+        strings.push_back(str);
+    } while (next_permutation(str.begin(), str.end())); 
+    
+    for(int i = 0; i<strings.size();i++){
+        
+        if(strings[i][0] > 50){
+            continue;
+        }
+        if(strings[i][2] > 53){
+            continue;
+        }
+        if(strings[i][0] == 50 && strings[i][1] > 51){
+            continue;
+        }
+        finals.push_back(strings[i]);
+    }
+    
+    if(finals.size() == 0){
+        return "";
+    }
+   for( int i = 0; i< finals.size();i++){
+    stringstream str(finals[i]); 
+    str >> tempInt;
+    numbers.push_back(tempInt);  
+   }
+    tempInt = *std::max_element(numbers.begin(),numbers.end());
+    finalstr = to_string(tempInt);
+    
+    while(finalstr.length() < 4){
+        finalstr.insert(0,"0");
+    }
+    for( int i = 0; i<4;i++){
+        newStr+=finalstr[i];
+        if(i==1){ newStr+=":";}
+    }
+    return newStr;
+} 
 };
